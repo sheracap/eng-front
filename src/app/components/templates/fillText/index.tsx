e1e5fa3fde7397a0b91a3ac2304bdb2b74925eb2
Sherzod Tursunov <shtursunov@uzkassa.uz>
2023-12-27 18:34:05 +0500

    fill text answer

--- src/app/components/templates/fillText/index.tsx
+++ src/app/components/templates/fillText/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from "react";
+import React, { FC, useMemo, useRef } from "react";
 import { ExerciseItemModel } from "#businessLogic/models/section";
 
 import styles from "./styles.module.scss";
@@ -10,16 +10,34 @@ type PropsTypes = {
 export const TemplateFillText: FC<PropsTypes> = (props) => {
   const { data } = props;
 
+  const answer = useRef({});
+
   const text = useMemo(() => {
     return JSON.parse(data.value);
   }, []);
+
+  const onChange = (e) => {
+    const key = 0;
+    const item = "";
+    const value = e.currentTarget.textContent;
+
+
+    answer.current = {
+      ...answer.current,
+      [key]: {
+        value,
+        isCorrect: item === value
+      }
+    };
+
+  };
 
   return (
     <div className={styles.fillTextTemplate}>
       {text.map((item, index) => (
         <React.Fragment key={index}>
           {item[0] === "[" && (
-            <span className={styles.fillTextInput} contentEditable={true} />
+            <span className={styles.fillTextInput} onInput={onChange} data-val={{ index, item }} contentEditable={true} />
           )}
 
           {item[0] !== "[" && (
